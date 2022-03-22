0dc58ba15c66b03df4b1eaf488b0ff1453dd0a90
AydinTuna <tunaaydin6161@gmail.com>
2022-03-22 21:15:36 +0300

    Proje dosyası ekle.

---
+++ Linked List - 2/List.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List___2
+{
+    public class List
+    {
+        public Node head;
+
+        public List()
+        {
+            head = null;
+        }
+
+        public void sonaEkle(int data)
+        {
+            Node eleman = new Node(data);
+
+            if(head == null)
+            {
+                head = eleman;
+                Console.WriteLine("Liste oluşturuldu, ilk düğüm eklendi");
+            }
+            else
+            {
+                Node temp = head;
+
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = eleman;
+                Console.WriteLine("Sona eleman eklendi");
+            }
+        }
+
+        public void basaEkle(int data)
+        {
+            Node eleman = new Node(data);
+
+            if(head == null)
+            {
+                head = eleman;
+            }
+            else
+            {
+                eleman.next = head;
+                head = eleman;
+
+                Console.WriteLine("Başa eleman eklendi");
+            }
+        }
+
+        public void yazdir()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Yazdırılacak bir değer yok");
+            }
+            else
+            {
+                Node eleman = head;
+                Console.Write("Baş -> ");
+                while (eleman.next != null)
+                {
+                    Console.Write(eleman.data + " -> ");
+                    eleman = eleman.next;
+                }
+                Console.Write(eleman.data + " son.");
+            }
+        }
+
+        public void bastanSil()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Liste boş");
+            }
+            else
+            {
+                head = head.next;   // Baştaki düğüm silindi
+                Console.WriteLine("Baştaki eleman silindi");
+            }
+        }
+
+        public void sondanSil()
+        {
+            Node temp = head;
+            Node temp2 = temp;
+            if (head == null)
+            {
+                Console.WriteLine("Liste boş");
+            }
+            else if (head.next == null)
+            {
+                head = null;
+                Console.WriteLine("Sondaki eleman silindi");
+            }
+            else
+            {
+                while(temp.next != null)
+                {
+                    temp2 = temp;
+                    temp = temp.next;
+                }
+                temp2.next = null;
+                Console.WriteLine("Sondaki eleman silindi");
+            }
+        }
+    }
+}
---
+++ Linked List - 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using Linked_List___2;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List tyList = new List();
+
+            int sayi;
+            int secim = menu();
+
+            while(secim != 0)
+            {
+                switch (secim)
+                {
+                    case 1:
+                        Console.Write("Başa sayı ekleyin: "); sayi = int.Parse(Console.ReadLine());
+                        tyList.basaEkle(sayi);
+                        tyList.yazdir();
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        Console.Write("Sona sayı ekleyin: "); sayi = int.Parse(Console.ReadLine());
+                        tyList.sonaEkle(sayi);
+                        tyList.yazdir();
+                        Console.WriteLine("\n");
+                        break;
+                    case 3:
+                        Console.Write("Baştan silin: "); tyList.bastanSil();
+                        tyList.yazdir();
+                        Console.WriteLine("\n");
+                        break;
+                    case 4:
+                        Console.Write("Sondan silin: "); tyList.sondanSil();
+                        tyList.yazdir();
+                        Console.WriteLine("\n");
+                        break;
+                    case 0:
+                        Console.WriteLine("\n");
+                        break;
+                }
+                secim = menu();
+            }
+        }
+
+        private static int menu()
+        {
+            int secim;
+
+            Console.WriteLine("1 - başa ekle");
+            Console.WriteLine("2 - sona ekle");
+            Console.WriteLine("3 - baştan sil");
+            Console.WriteLine("4 - sondan sil");
+            Console.WriteLine("0 - programı kapat");
+            Console.WriteLine("Seçiminiz: ");
+            secim = int.Parse(Console.ReadLine());
+
+            return secim;
+        }
+    }
+}
